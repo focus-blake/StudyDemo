3419eb5575d8d92c92e753e0a965d64464236263
focus-blake <81617055+focus-blake@users.noreply.github.com>
2021-03-31 14:07:58 +0800

    xmlTest

---
+++ XmlTest/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace XmlTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World!");
+        }
+
+        /// <summary>
+        /// 将person对象保存为xml文件
+        /// </summary>
+        /// <param name="p"></param>
+        static void SavePerson(Person p)
+        { 
+        }
+
+        /// <summary>
+        /// 从指定的xml文件读取person对象
+        /// </summary>
+        /// <param name="xmlPath"></param>
+        static void ReadPerson(string xmlPath)
+        {
+            
+        }
+    }
+
+    class Person
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Hometown { get; set; }
+    }
+}
