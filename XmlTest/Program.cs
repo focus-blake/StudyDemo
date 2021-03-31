f17f258fe2d3449c2604cf6c269aeaaa4ba49e28
focus-blake <81617055+focus-blake@users.noreply.github.com>
2021-03-31 15:53:07 +0800

    xml finished

--- XmlTest/Program.cs
+++ XmlTest/Program.cs
@@ -8,13 +8,14 @@ namespace XmlTest
     {
         static void Main(string[] args)
         {
-            Person p = new Person();
-            p.Id = Guid.NewGuid().ToString();
-            p.Name = "诸葛亮";
-            p.Age = 40;
-            p.Hometown = "隆中";
+            //Person p = new Person();
+            //p.Id = Guid.NewGuid().ToString();
+            //p.Name = "诸葛亮";
+            //p.Age = 40;
+            //p.Hometown = "隆中";
 
-            SavePerson(p,@"D:/Person.xml");
+            //SavePerson(p,@"D:/Person.xml");
+            DelPersonById("2");
             Console.WriteLine("end!");
         }
 
@@ -57,9 +58,60 @@ namespace XmlTest
         /// 从指定的xml文件读取person对象
         /// </summary>
         /// <param name="xmlPath"></param>
-        static void ReadPerson(string xmlPath)
+        static void ReadPerson()
         {
-            
+            XDocument doc = XDocument.Load(@"D:/Person.xml");
+            var result = from d in doc.Descendants("Person")
+                         select new
+                         {
+                             id = d.Attribute("Id").Value,
+                             name = d.Element("Name").Value,
+                             age = d.Element("Age").Value,
+                             hometown = d.Element("Hometown").Value
+                         };
+
+            foreach (var p in result)
+            {
+                Console.WriteLine(p.id);
+                Console.WriteLine(p.name);
+                Console.WriteLine(p.age);
+                Console.WriteLine(p.hometown);
+            }
+        }
+
+        /// <summary>
+        /// 在xml文件子节点下增加属性和数据
+        /// </summary>
+        static void AddXmlAttribute()
+        {
+            XDocument doc = XDocument.Load(@"D:/Person.xml");
+            // 在Person节点添加属性和数据
+            doc.Root.Element("Person").Add(new XElement("Sister","Amy"));
+            doc.Save(@"D:/Person.xml");
+        }
+
+        /// <summary>
+        /// 根据Id删除Person节点
+        /// </summary>
+        /// <param name="id"></param>
+        static void DelPersonById(string id)
+        {
+            XDocument doc = XDocument.Load(@"D:/Person.xml");
+            try
+            {
+                // 获取目标节点
+                XElement target = doc.Root.Elements("Person").Where(t => t.Attribute("Id").Value.Equals(id)).Single();
+                // 删除节点
+                if (target != null)
+                {
+                    target.Remove();
+                    doc.Save(@"D:/Person.xml");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("存在多个符合条件的xml节点" + e.Message);
+            } 
         }
     }
 
