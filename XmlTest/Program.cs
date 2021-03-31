1f53cc610455acf50651d220f593ab27ca82594b
focus-blake <81617055+focus-blake@users.noreply.github.com>
2021-03-31 14:38:14 +0800

    SavePerson

--- XmlTest/Program.cs
+++ XmlTest/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Xml.Linq;
 
 namespace XmlTest
 {
@@ -6,16 +8,49 @@ namespace XmlTest
     {
         static void Main(string[] args)
         {
+            Person p = new Person();
+            p.Id = Guid.NewGuid().ToString();
+            p.Name = "诸葛亮";
+            p.Age = 40;
+            p.Hometown = "隆中";
 
-            Console.WriteLine("Hello World!");
+            SavePerson(p,@"D:/Person.xml");
+            Console.WriteLine("end!");
         }
 
         /// <summary>
         /// 将person对象保存为xml文件
         /// </summary>
         /// <param name="p"></param>
-        static void SavePerson(Person p)
-        { 
+        static void SavePerson(Person p,string outPath)
+        {
+            XDocument doc = new XDocument
+            (
+                new XDeclaration("1.0", "utf-8", "yes"),
+                // 根节点
+                new XElement
+                // 根节点名称
+                ("PersonGroup",
+                     // 子节点
+                     new XElement
+                     ("Person",
+                     // 子节点的属性
+                     new XAttribute("Id", "1"),
+                     new XElement("Name", p.Name),
+                     new XElement("Age", p.Age),
+                     new XElement("Hometown", p.Hometown)
+                     ),
+                     // 子节点
+                     new XElement
+                     ("Person",
+                     new XAttribute("Id", "2"),
+                     new XElement("Name", p.Name),
+                     new XElement("Age", p.Age),
+                     new XElement("Hometown", p.Hometown)
+                     )
+                )
+            );
+            doc.Save(outPath);
         }
 
         /// <summary>
